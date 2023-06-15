dd05bc3d70cd9cde39c7eff680fc30ec95d0fc7e
mrtaz77 <mrtazwar23@gmail.com>
2023-06-15 22:41:32 +0600

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+//set up template engine
+app.set('view engine','ejs')
+app.use(cors());
+app.use(express.json());
+
+// static files
+app.use('/assets',express.static('./public/assets'))
+
+
+//listen to port
+app.listen(3000, () => console.log("App is listening on port 3000"));
