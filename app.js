44820716454553287adfc23f1b7efa3f082a7bee
mr Tazwar <113765142+mrtaz77@users.noreply.github.com>
2023-08-24 10:52:29 +0600

    Revert "Icarus77"

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors(
         origin: '*',
         methods: "GET,POST,PUT,DELETE, PATCH",
         credentials: true,
-        maxAge: 900000,
+        maxAge: 36000,
     }
 ));
 app.use(express.json());
