d2c2975de845c2de281ba36f070332242e2f3f04
mrtaz77 <mrtazwar23@gmail.com>
2023-08-28 22:46:47 +0600

    modified login

--- route/login.js
+++ route/login.js
@@ -3,49 +3,50 @@ const loginController = require('../controller/login');
 const router = express.Router({mergeParams: true});
 const authUtils = require('./authUtil');
 
-router.get('/', (req,res) => {
-    if(req.user === null){
-        const errors = [];
-        // 
-        
-        // 
-    }else {
-        console.log(`${req.user.name} logged`);
-    }
-});
+
 
 router.post('/', async (req, res) => {
-    if (req.user == null){
+    if (req.user == null) {
         let results, errors = [];
-        // get login info for handle (id, handle, password)
-        results = await loginController.getUserLoginInfoByName(req.body.name);
-        console.log(`Got results in login router`);
-
-        if(results.length === 0){
-            errors.push(`No such user found`);
+        
+        results = await loginController.getUserLoginInfoByName(req.body.username);
+        
+        
+        try {
+            if (results.length === 0) {
+                errors.push(`No such user found`);
+            }
+        } catch (error) {
+            return res.status(400).json({ error: 'An error occurred' });
         }
-        else{
-            const flag = await loginController.chkCreds(req.body.name,req.body.password);
-            if (flag == 1337){
-                console.log(`${req.body.name} logged in successfully...`);
-                await authUtils.loginUser(res,req.body.name);
-            }else {
+        
+        
+        //else {
+            const flag = await loginController.chkCreds(req.body.username, req.body.password);
+            if (flag == 1337) {
+                console.log(`${req.body.username} ${results[0].TYPE} logged in successfully...`);
+                await authUtils.loginUser(res, req.body.username, results[0].TYPE);
+
+                // Send a JSON response indicating successful login
+                return res.json({
+                    message: 'Login successful',
+                    userId: req.body.username,
+                    userType: results[0].TYPE,
+                });
+                //return res.json({ message: 'Login successful' });
+            } else {
                 errors.push(`Invalid login`);
             }
-        }   
+       // }
 
-        // if any error, redirect to login page but with form information, else redirect to homepage
-        if (errors.length == 0) {
-            console.log(`No error found , continuing to dashboard...`);
-            // console.log(`/user/${results[0].TYPE}`);
-            res.json(results);
-            
-            // res.redirect(`/user/${results[0].TYPE}`);
-        } 
-
+        // If there are errors, send a JSON response with the errors
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
+        }
+    } else {
+        // Send a JSON response indicating the user is already logged in
+        return res.json({ message: 'User is already logged in' });
     }
 });
-
-
 
 module.exports = router;
--- route/logout.js
+++ route/logout.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 
-const port = process.env.PORT || 3000;
-
-
 router.post('/', async (req, res) => {
     res.clearCookie("userSessionToken");
-    console.log("Logged out successfully");
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 module.exports = router;
