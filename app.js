465abf23c1cf93ba35538e5201c8ce98bc374e6f
mrtaz77 <mrtazwar23@gmail.com>
2023-11-01 15:43:58 +0600

    update app.js

--- app.js
+++ app.js
@@ -28,16 +28,19 @@ app.use(cors(
 app.use(express.json());
 app.use(auth);
 
+app.use(express.static('public'));
+
 // using routes and linking them to routers 
 app.use('/login',require('./route/login'));
 app.use('/reg', require('./route/reg'));
 app.use('/stock', require('./route/stock'));
 app.use('/activity', require('./route/activity'));
-
-// For testing purposes only
+app.use('/logout',require('./route/logout'));
+app.use('/participate', require('./route/participate'));
 app.use('/admin', require('./route/admin'));
 app.use('/user', require('./route/user'));
 app.use('/order', require('./route/order'));
+app.use('/broker', require('./route/broker'));
 
 app.use(errHandler.notFound);
 app.use(errHandler.errHandler);
